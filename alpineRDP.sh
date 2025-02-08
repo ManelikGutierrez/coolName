b7b4c28aae7bfd67f7e78e9fa7ebb59a572ff681
ManelikGutierrez <manelik.gutierrez.jr@gmail.com>
2025-02-07 22:40:40 -0800

    removed Sudo syntax

--- alpineRDP.sh
+++ alpineRDP.sh
@@ -1,7 +1,7 @@
-sudo apk update 
-sudo apk upgrade
-sudo apk add xrdp xorg-server xfce4 xfce4-terminal dbus-x11
-sudo rc-update add xrdp default
-sudo service xrdp start
-sudo iptables -A INPUT -p tcp --dport 3389 -j ACCEPT
-sudo ufw allow 3389/tcp
+apk update 
+apk upgrade
+apk add xrdp xorg-server xfce4 xfce4-terminal dbus-x11
+rc-update add xrdp default
+service xrdp start
+iptables -A INPUT -p tcp --dport 3389 -j ACCEPT
+ufw allow 3389/tcp
--- ufwSetupAlpine.sh
+++ ufwSetupAlpine.sh
@@ -1,30 +1,30 @@
 echo "Installing UFW..."
-sudo apk add ufw
+apk add ufw
 
 # Enable UFW service
 echo "Enabling UFW service..."
-sudo rc-update add ufw default
-sudo service ufw start
+rc-update add ufw default
+service ufw start
 
 # Allow SSH (port 22)
-echo "Allowing SSH..."
+"Allowing SSH..."
 sudo ufw allow 22/tcp
 
 # Allow RDP (port 3389) - Optional
 echo "Allowing RDP..."
-sudo ufw allow 3389/tcp
+ufw allow 3389/tcp
 
 # Enable UFW default rules
 echo "Setting default policies..."
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
+ufw default deny incoming
+ufw default allow outgoing
 
 # Enable UFW
 echo "Enabling UFW..."
-yes | sudo ufw enable
+yes | ufw enable
 
 # Verify UFW status
 echo "UFW status:"
-sudo ufw status verbose
+ufw status verbose
 
 echo "Setup complete. SSH and RDP are now allowed."
